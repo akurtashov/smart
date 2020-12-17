0385d64804d6c61b8c52149c522a8f10400a40d3
arthur <6036916@mail.ru>
2020-12-17 08:15:12 +0300

    Command string arguments is adding

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 import configparser
 import os
+import argparse
+
 
 CONFIG_FILE = os.path.normpath('C:\etc\smart.conf')
 
 
 def setup():
-    print(CONFIG_FILE)
+    parser = argparse.ArgumentParser(description='Setup initial parameter of smart project')
+    parser.add_argument('-c', default='/etc/smart.conf', type=str, help='config file name')
+    
+    args = parser.parse_args()
+    
+    config_file = args.c
+
+    print(config_file)
     config = configparser.ConfigParser()
-    config.read(CONFIG_FILE)
+    config.read(config_file)
     ftp_server = config['server']
     print(ftp_server.get('ip'))
 
